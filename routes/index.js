21dc76f29a81ec6ebbc39354eb2b82ee8b120b79
Enrique <ecvoracle@gmail.com>
2021-07-13 22:21:20 +0100

    Menu: Added dropdown

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', {page:'Home', menuId:'home'});
 });
 
-router.get('/instructions', function(req, res, next) {
-  res.render('instructions', {page:'Instructions', menuId:'instructions'});
-});
-
 router.get('/register', function(req, res, next) {
   res.render('registerBike', {page:'Register Bike', menuId:'register'});
 });
 
-router.get('/management', function(req, res, next) {
-  res.render('management', {page:'Management', menuId:'management'});
+router.get('/showdetails', function(req, res, next) {
+  res.render('showDetails', {page:'Show details', menuId:'showdetails'});
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About', menuId:'about'});
+router.get('/adddetails', function(req, res, next) {
+  res.render('addDetails', {page:'Add details', menuId:'adddetails'});
+});
+
+router.get('/transferownership', function(req, res, next) {
+  res.render('transferOwnership', {page:'Transfer Ownership', menuId:'transferownership'});
+});
+
+router.get('/renounceownership', function(req, res, next) {
+  res.render('renounceOwnership', {page:'Renounce Ownership', menuId:'renounceownership'});
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', {page:'FAQ', menuId:'faq'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact', menuId:'contact'});
 });
 
 module.exports = router;
