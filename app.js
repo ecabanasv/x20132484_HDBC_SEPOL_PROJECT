55c6b1322943a0f1f434aeadbe07c86ce924881a
Enrique <ecvoracle@gmail.com>
2021-07-13 20:18:10 +0100

    Menu: Added menu & initial css

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,10 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/cssb', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/jsb', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/instructions', function(req, res, next) {
+  res.render('instructions', {page:'Instructions', menuId:'instructions'});
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('registerBike', {page:'Register Bike', menuId:'register'});
+});
+
+router.get('/management', function(req, res, next) {
+  res.render('management', {page:'Management', menuId:'management'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About', menuId:'about'});
 });
 
 module.exports = router;
