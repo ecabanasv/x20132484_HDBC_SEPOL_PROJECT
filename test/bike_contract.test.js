daaa483e29f4ee4564cb4d1857a4427b65573c3a
Enrique <ecvoracle@gmail.com>
2021-07-19 19:52:12 +0100

    Truffle: 2nd test showListBikeDetails()

--- test/bike_contract.test.js
+++ test/bike_contract.test.js
@@ -1,4 +1,5 @@
 const BikeContract = artifacts.require('BikeContract');
+const { assert } = require('chai');
 const truffleAssert = require('truffle-assertions');
 
 /*
@@ -30,47 +31,60 @@ contract("BikeContract", function (accounts) {
   });
   describe("@ Bike and owner listing", () => {
     /* showListBikeDetails */
-
-    it("showListBikeDetails: Can show the list of bikes registered", async () => {});
-
-    /* showBikeDetails */
-
-    it("showBikeDetails: Can show a specific bike registered", async () => {});
+    it("showListBikeDetails: Can show the list of bikes registered", async () => {
+      const instance = await BikeContract.deployed();
+      const bike1 = await instance.newBike("Orbea", "2019", "123123", "-", "Enrique", "ecvoracle@gmail.com", {from:accounts[0]});
+      const bike2 = await instance.newBike("Carrefour", "2016", "892342", "-", "Juan", "juan@gmail.com", {from:accounts[0]});
+      const bike3 = await instance.newBike("Nike", "2010", "312456", "-", "Pepe", "pepe@gmail.com", {from:accounts[0]});
+      const bike4 = await instance.newBike("Adidas", "2004", "345345", "-", "Luis", "luis@gmail.com", {from:accounts[0]});
+      const bikeList = await instance.showListBikeDetails();
+      //Bike 1: Orbea
+      assert.equal(bikeList[1][1], "Orbea");
+      //Bike 2: 2016
+      assert.equal(bikeList[2][2], "2016");
+      //Bike 3: 312456
+      assert.equal(bikeList[3][3], "312456");
+      //Bike 4: -
+      assert.equal(bikeList[4][4], "-");
+    });
   });
 
+  /* showBikeDetails */
+
+  it("showBikeDetails: Can show a specific bike registered", async () => {});
   describe("@ Owner functions", () => {
     /* _newOwner */
-
+  
     it("_newOwner: Can register new owner", async () => {});
-
+  
     /* updateOwner */
-
+  
     it("updateOwner: Can update a specific registered owner", async () => {});
-
+  
     /* showListOwnerDetails */
-
+  
     it("showListOwnerDetails: Can show a specific owners registered", async () => {});
-
+  
     /* showListOwnerDetails */
-
+  
     it("showListOwnerDetails: Can show the list of owners registered", async () => {});
-
+  
     /* showOwnerDetails */
-
+  
     it("showOwnerDetails: Can show a specific owner registered", async () => {});
-  });
-
+    });
+  
   describe("@ Add details, transferOwnership and renounceOwnership functions", () => {
     /* addDetails */
-
+  
     it("addDetails: Can update bike details", async () => {});
-
+  
     /* transferOwnership */
-
+  
     it("transferOwnership: Can transfer ownership to future owner", async () => {});
-
+  
     /* renounceOwnership */
-
+  
     it("renounceOwnership: User can renounce its bike ownership", async () => {});
   });
 });
